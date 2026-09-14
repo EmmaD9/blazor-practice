80a022e67f8f43845922dff97794ad48abd3e587
Emma Duprey <maduprey1@gmail.com>
2026-09-14 17:09:29 -0400

    adding form template

---
+++ Prototype01/Components/Models/FormData.cs
@@ -0,0 +1,9 @@
+namespace Prototype01.Models;
+
+public class FormData
+{
+    public string? Field1 { get; set; }
+    public string? Field2 { get; set; }
+    public string? Field3 { get; set; }
+    public string? Field4 { get; set; }
+}
